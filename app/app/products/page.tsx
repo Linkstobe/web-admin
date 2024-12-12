0566d133c5e06514ab804b558d4b43b7387e1b0c
Linkstobe <linkstobe@gmail.com>
2024-12-12 00:43:59 -0300

    refact: optimizes data fetching on the product screen

--- app/app/products/page.tsx
+++ app/app/products/page.tsx
@@ -8,8 +8,8 @@ import { ITransaction } from "@/interfaces/ITransactions";
 export default async function Products () {
   const token = getCookie("authToken")
 
-  const productAccessMetrics: IMetric[] = []
-  const productClicksMetrics: IMetric[] = []
+  let productAccessMetrics: IMetric[] = []
+  let productClicksMetrics: IMetric[] = []
 
   let allProducts: IProduct[] = []
   let allTransactions: ITransaction[] = []
@@ -21,11 +21,20 @@ export default async function Products () {
     const metricsUrl = `${process.env.NEXT_PUBLIC_API_URL}/metrics`;
     const transactionsUrl = `${process.env.NEXT_PUBLIC_API_URL}/transactions`;
 
+    const productAccessMetricsUrl = `${process.env.NEXT_PUBLIC_API_URL}/metrics/type/view:product`
+    const carouselProductClickMetricsUrl = `${process.env.NEXT_PUBLIC_API_URL}/metrics/type/click:panel-Carrossel`
+    const blockProductClicksMetricsUrl = `${process.env.NEXT_PUBLIC_API_URL}/metrics/type/click:panel-Bloco`
+    const galeryProductClicksMetricsUrl = `${process.env.NEXT_PUBLIC_API_URL}/metrics/type/click:panel-Galeria`
+
     const [
       projectsResponse,
       productsResponse,
-      metricsResponse,
+      // metricsResponse,
       transactionsResponse,
+      carouselProductClicksResponse,
+      blockProductClicksResponse,
+      galeryProductClicksResponse,
+      productAccessResponse
     ] = await Promise.all([
       fetch(projectsUrl, {
         headers: { Authorization: `Bearer ${token}` }
@@ -33,10 +42,22 @@ export default async function Products () {
       fetch(productsUrl, {
         headers: { Authorization: `Bearer ${token}` }
       }),
-      fetch(metricsUrl, {
+      // fetch(metricsUrl, {
+      //   headers: { Authorization: `Bearer ${token}` }
+      // }),
+      fetch(transactionsUrl, {
         headers: { Authorization: `Bearer ${token}` }
       }),
-      fetch(transactionsUrl, {
+      fetch(carouselProductClickMetricsUrl, {
+        headers: { Authorization: `Bearer ${token}` }
+      }),
+      fetch(blockProductClicksMetricsUrl, {
+        headers: { Authorization: `Bearer ${token}` }
+      }),
+      fetch(galeryProductClicksMetricsUrl, {
+        headers: { Authorization: `Bearer ${token}` }
+      }),
+      fetch(productAccessMetricsUrl, {
         headers: { Authorization: `Bearer ${token}` }
       }),
     ])
@@ -44,40 +65,51 @@ export default async function Products () {
     const requestFailed = 
       !projectsResponse.ok || 
       !productsResponse.ok || 
-      !metricsResponse.ok || 
-      !transactionsResponse.ok
+      // !metricsResponse.ok || 
+      !transactionsResponse.ok ||
+      !carouselProductClicksResponse.ok ||
+      !blockProductClicksResponse.ok ||
+      !galeryProductClicksResponse.ok ||
+      !productAccessResponse.ok
 
     if (requestFailed) {
       throw new Error("Falha em uma ou mais requisições");
     }
 
-    const [projects, products, metrics, transactions]: [IProject[], IProduct[], IMetric[], ITransaction[]] = await Promise.all([
+    const [projects, products, carouselProductClicks, blockProductClicks, galeryProductClicks, productAccess, transactions]: 
+      [IProject[], IProduct[], IMetric[], IMetric[], IMetric[], IMetric[], ITransaction[]] = await Promise.all([
       projectsResponse.json(),
       productsResponse.json(),
-      metricsResponse.json(),
+      // metricsResponse.json(),
+      carouselProductClicksResponse.json(),
+      blockProductClicksResponse.json(),
+      galeryProductClicksResponse.json(),
+      productAccessResponse.json(),
       transactionsResponse.json(),
     ])
 
     allProjects = projects
     allProducts = products
     allTransactions = transactions
+    productAccessMetrics = productAccess
+    productClicksMetrics = [...carouselProductClicks, ...blockProductClicks, ...galeryProductClicks]
 
-    metrics.forEach((metric) => {
-      const { link_type } = metric
+    // metrics.forEach((metric) => {
+    //   const { link_type } = metric
 
-      const isProductClickMetric = 
-        link_type.startsWith("click:panel-Carrossel") ||
-        link_type.startsWith("click:panel-Galeria") ||
-        link_type.startsWith("click:panel-Bloco")
+    //   const isProductClickMetric = 
+    //     link_type.startsWith("click:panel-Carrossel") ||
+    //     link_type.startsWith("click:panel-Galeria") ||
+    //     link_type.startsWith("click:panel-Bloco")
 
-      const isProductAccessMetric = link_type.startsWith("view:product")
+    //   const isProductAccessMetric = link_type.startsWith("view:product")
       
-      if (isProductAccessMetric) {
-        productAccessMetrics.push(metric)
-      } else if (isProductClickMetric) {
-        productClicksMetrics.push(metric)
-      }
-    })
+    //   if (isProductAccessMetric) {
+    //     productAccessMetrics.push(metric)
+    //   } else if (isProductClickMetric) {
+    //     productClicksMetrics.push(metric)
+    //   }
+    // })
 
 
   } catch (error) {
